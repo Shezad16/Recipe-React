d2268c4f2cf086e929712bbf134893917be31004
Shezad Rayeen <shejadrayeen02@gmail.com>
2025-12-02 11:23:49 +0000

    commit 2

--- my-app/src/components/Recipe.jsx
+++ my-app/src/components/Recipe.jsx
@@ -43,21 +43,21 @@ export default function Recipe()
         <div style={{maxWidth:"600px",margin:"20px auto"}}>
             <h1 style={{marginBottom:"20px",border:"2px solid #030303ff",padding:"20px", textAlign:"center",color:"black",backgroundColor:"#F54927",}}>Recipe Book</h1>
             
-            <form>
+            <form onSubmit={handleSubmit}> 
 
             <div style={{marginBottom:"10px"}}>
             <label>Name:</label><br />
-            <input type="text" style={{width:"100%",padding:"5px",border:"1.2px solid #030303ff"}} />
+            <input type="text" style={{width:"100%",padding:"5px",border:"1.2px solid #030303ff"}} value={recipename} onChange={(e)=>setRecipeName(e.target.value)} />
             </div>
 
             <div style={{marginBottom:"10px"}}>
             <label>Ingredients:</label><br />
-            <input type="text" style={{width:"100%",padding:"5px",border:"1.2px solid #030303ff"}} />
+            <input type="text" style={{width:"100%",padding:"5px",border:"1.2px solid #030303ff"}} value={ingredients} onChange={(e)=>setIngredients(e.target.value)} />
             </div>
 
             <div style={{marginBottom:"10px"}}>
             <label>Instructions:</label><br />
-            <input type="text" style={{width:"100%",padding:"5px",border:"1.2px solid #030303ff"}} />
+            <input type="text" style={{width:"100%",padding:"5px",border:"1.2px solid #030303ff"}} value={instructions} onChange={(e)=>setInstructions(e.target.value)} />
             </div>
 
             <button style={{padding:"5px 10px",border:"1.2px solid #030303ff"}} type="submit">
